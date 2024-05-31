eec200f3e705029c9ee193df997ef40b73e05229
helderscrolls <nortehelder@gmail.com>
2024-05-31 23:44:31 +0200

    style: add dark mode to landing page

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -8,16 +8,31 @@ export const Heroes = () => {
           <Image
             src="/documents.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
             alt="Documents"
           />
+
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Documents"
+          />
+
         </div>
 
         <div className="relative h-[400px] w-[400px] hidden md:block">
           <Image
             src="/reading.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Reading"
+          />
+
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Reading"
           />
         </div>
--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -16,7 +16,17 @@ export const Logo = () => {
         height="40"
         width="40"
         alt="Logo"
+        className="dark:hidden"
       />
+
+      <Image
+        src="/logo-dark.svg"
+        height="40"
+        width="40"
+        alt="Logo"
+        className="hidden dark:block"
+      />
+
 
       <p className={cn("font-semibold", font.className)}>
         Jotion
