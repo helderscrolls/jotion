979855b8281eba510e25b6b1bf4dd364b0e5de79
helderscrolls <nortehelder@gmail.com>
2024-06-01 00:24:54 +0200

    feat(auth): add spinner for auth, made auth buttons dynamic, add UserButton to manage account

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ConvexClientProvider } from "../components/providers/convex-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
