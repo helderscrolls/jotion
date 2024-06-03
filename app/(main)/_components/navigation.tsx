688bf5c6d1ef90207839837faa12e0b381345e72
helderscrolls <nortehelder@gmail.com>
2024-06-03 21:55:39 +0200

    feat(Documents): add basic api for adding and fetching documents with Convex, display said documents on sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
+import { useQuery } from "convex/react";
 import { ChevronsLeftIcon, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
+  const documents = useQuery(api.documents.get);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -113,11 +117,13 @@ export const Navigation = () => {
         </div>
 
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
 
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document._id}>{document.title}</p>
+          ))}
         </div>
 
         <div
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,13 +16,13 @@ export const metadata: Metadata = {
         url: "/logo.svg",
         href: "/logo.svg",
       },
-       {
+      {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
         href: "/logo-dark.svg",
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -40,10 +40,11 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
             storageKey="jotion-theme"
-            >
+          >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
-      </ConvexClientProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
